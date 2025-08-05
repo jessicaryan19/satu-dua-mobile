5b817c070aee5cfe0bf74b850e2fa901889a3c19
Jessica Ryan <jessicaryan0419@gmail.com>
2025-08-06 04:31:30 +0700

    feat: sign in page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,26 +1,21 @@
 import { View } from "react-native";
-import Illustration from "@/assets/illustration/hello.svg"
+import Illustration from "@/assets/illustration/peeking.svg"
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import ThemedTextInput from "@/components/ThemedTextInput";
 import ThemedPhoneInput from "@/components/ThemedPhoneInput";
 import ThemedPressable from "@/components/ThemedPressable";
 
-export default function SignUpScreen() {
+export default function SignInScreen() {
     return (
         <ThemedView className="flex justify-center items-center h-full gap-7">
             <View className="flex justify-center items-center">
-                <Illustration width={250} height={250} />
-                <ThemedText type="title" className="text-primary">Selamat Datang di SatuDua</ThemedText>
-                <ThemedText >Buat akunmu agar kami dapat segera melayanimu.</ThemedText>
+                <Illustration width={350} />
+                <ThemedText type="title" className="text-primary">Selamat Datang Kembali!</ThemedText>
+                <ThemedText >Yuk lanjutkan aktivitasmu.</ThemedText>
             </View>
 
             <View className="w-full flex gap-4">
-                <View className="w-full">
-                    <ThemedText type="defaultSemiBold">Siapa Nama Lengkapmu?</ThemedText>
-                    <ThemedTextInput placeholder="Nama Lengkap" />
-                </View>
-
                 <View className="w-full">
                     <ThemedText type="defaultSemiBold">Masukkan Nomor Telepon Aktif Kamu</ThemedText>
                     <ThemedPhoneInput placeholder="Nomor Telepon" />
@@ -33,10 +28,10 @@ export default function SignUpScreen() {
 
             <View className="flex flex-row items-center justify-center gap-1">
                 <ThemedText>
-                    Sudah punya akun?
+                    Belum punya akun?
                 </ThemedText>
                 <ThemedText type="link" className="text-primary">
-                    Masuk
+                    Daftar Sekarang
                 </ThemedText>
             </View>
         </ThemedView>
