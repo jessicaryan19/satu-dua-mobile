87c48651236d9371ed21279340c590371fe44d33
Jessica Ryan <jessicaryan0419@gmail.com>
2025-08-24 18:06:51 +0700

    feat: education section

--- components/cards/IconCards.tsx
+++ components/cards/IconCards.tsx
@@ -6,20 +6,22 @@ import { useRouter } from "expo-router";
 type IconCardProps = {
     icon: ReactNode,
     name: string,
-    tag: string
+    tag: string,
+    disabled?: boolean
 }
 
 export default function IconCards({
     icon,
     name,
     tag,
+    disabled = false
 }: IconCardProps) {
     const router = useRouter()
     const handlePress = () => {
         router.push({ pathname: '/nearby-help', params: { name: name, tag: tag } })
     }
     return (
-        <Pressable className='flex justify-center items-center w-1/5 gap-2' onPress={handlePress}>
+        <Pressable className='flex justify-center items-center w-1/5 gap-2' onPress={handlePress} disabled={disabled}>
             <View className='bg-accent w-16 h-16 rounded-2xl flex items-center justify-center'>
                 {icon}
             </View>
